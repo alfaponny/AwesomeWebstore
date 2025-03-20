2a1b2757ecadf13b329821489f40171984a701f7
Helenes-Hub <hrydros@gmail.com>
2025-03-20 12:18:13 +0100

    Fixat popup formulär

--- scripts.js
+++ scripts.js
@@ -5,3 +5,32 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+let modal=document.getElementById("checkout");
+let openModal=document.querySelectorAll(".checkoutPress");
+let closeModal = document.querySelector(".close");
+let submitOrder=document.getElementById("orderDone")
+
+
+console.log("Connection ESTABLISHED", modal, openModal);
+console.log("Number of buttons found:", openModal.length);
+
+openModal.forEach(button => {
+    button.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default link action
+        modal.style.display = "block";
+    });
+});
+
+closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
