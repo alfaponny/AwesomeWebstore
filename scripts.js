3ac37418d70d90b28ab72f8e13cf11b5bfb299c4
Helenes-Hub <hrydros@gmail.com>
2025-03-21 11:29:43 +0100

    utökat formuläret, lagt actionlistener på allt förutom adresskriterium + submit knappen

--- scripts.js
+++ scripts.js
@@ -7,15 +7,17 @@
 // Use this file to add JavaScript to your project
 
 
+//variabler hämtade från index
 let modal=document.getElementById("checkout");
 let openModal=document.querySelectorAll(".checkoutPress");
 let closeModal = document.querySelector(".close");
 let submitOrder=document.getElementById("orderDone")
 
-
+//bara error testing på hemsidan (tryck f12 och välj console)
 console.log("Connection ESTABLISHED", modal, openModal);
 console.log("Number of buttons found:", openModal.length);
 
+//actionlistener till "köp" knapparna via variabeln ovan
 openModal.forEach(button => {
     button.addEventListener("click", function(event) {
         event.preventDefault();
@@ -23,14 +25,49 @@ openModal.forEach(button => {
     });
 });
 
+//actionlistener till krysset på popup
 closeModal.addEventListener("click", function() {
     modal.style.display = "none";
 });
 
+//actionlistener till hela sidan, om click != popup, displaya den inte
 window.addEventListener("click", function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
 });
+
+//actionlistener till fullname
+document.getElementById("fullName").addEventListener("input", function() {
+    let errorMessage = "";
+
+    if (!this.value.includes(" ")) {
+        errorMessage = "Please enter both first and last name.";
+    } else if (this.value.length < 3 || this.value.length>50) {
+        errorMessage = "Unsupported length";
+    }
+
+    this.setCustomValidity(errorMessage);
+});
+
+//actionlistener till phonenumber
+document.getElementById("phoneNumber").addEventListener("input", function() {
+    let errorMessage = "";
+
+    //regex
+    let phonePattern = /^[0-9\s\-()]+$/
+
+    if (!phonePattern.test(this.value)) {
+        errorMessage = "Please enter a valid phone number";
+    } else if (this.value.length>50) {
+        errorMessage = "Unsupported length";
+    }
+
+    this.setCustomValidity(errorMessage);
+});
+
+//          address: Min 2 tecken och Max 50 tecken
+//           ii. postalCode: Exakt 5 siffror
+//           iii. city: Min 2 tecken och Max 50 tecken
 
 
